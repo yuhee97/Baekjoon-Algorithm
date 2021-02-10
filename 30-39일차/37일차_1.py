90762bed55a0ae75e0d87b97496909271b8f91b1
yuhee97 <76423543+yuhee97@users.noreply.github.com>
2021-02-11 02:21:27 +0900

    Add files via upload

---
+++ 30-39일차/37일차_1.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)) :
+        for j in range(i+1,len(phone_book)) :
+            if phone_book[i] == phone_book[j][0:len(phone_book[i])] :
+                return False
+    return True
+
+lis = ["119", "97674223", "1195524421"]
+solution(lis)
