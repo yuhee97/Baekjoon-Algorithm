a84951f2a228e7c794be2c250a0c3b6c0159613d
yuhee97 <76423543+yuhee97@users.noreply.github.com>
2021-02-14 23:09:02 +0900

    Delete 39일차_행렬의덧셈.py

--- 39일차_행렬의덧셈.py
+++
@@ -1,9 +0,0 @@
-def solution(arr1, arr2):
-    answer = [[] for _ in range(len(arr1))]
-    for i in range(len(arr1)):
-        for j in range(len(arr1[i])):
-            SUM = 0
-            SUM += (arr1[i][j] + arr2[i][j])
-            answer[i].append(SUM)
-    return answer
-            
