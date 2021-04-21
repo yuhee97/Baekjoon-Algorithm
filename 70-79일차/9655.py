a63ed5c1e6d12aecb776d89b3177b10a78254961
yuhee97 <76423543+yuhee97@users.noreply.github.com>
2021-04-21 20:42:58 +0900

    Add files via upload

---
+++ 70-79일차/9655.py
@@ -0,0 +1,14 @@
+def game(n):
+    c = 0
+    while True:
+        c += 1
+        if n == 3 or n == 1:
+            if c % 2 == 1:
+                return "SK"
+                break
+            else:
+                return "CY"
+                break
+        n -= 1
+        
+print(game(int(input())))
