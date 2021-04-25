9edf88f18a3f75baf844fa5fb83906f30d415bf5
yuhee97 <76423543+yuhee97@users.noreply.github.com>
2021-04-26 00:22:31 +0900

    Update 1446.py

--- 70-79일차/1446.py
+++ 70-79일차/1446.py
@@ -1,28 +1,20 @@
-import heapq
 import sys
-    
-def dijkstra(start, distance):
-    q = []
-    heapq.heappush(q, [0, start])
-    while q:
-        print(q)
-        dist, now  = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in g[now]:
-            cost = i[1] + dist
-            if cost < distance[i[0]]:
-                print(1)
-                distance[i[0]] = cost
-                heapq.heappush(q, [cost, i[0]])
-    return distance[d]
 
-n, d = map(int, input().split())
-g = [[] for _ in range(10001)]
+def function(D, distance, graph):
+    for i in range(0, D+1):
+        if i != 0:
+            distance[i] = min(distance[i], distance[i-1]+1)
+        for a, b, c in graph:
+            if a == i and distance[i]+c < distance[b]:
+                distance[b] = distance[i]+c
+    return distance[D]
+
+n, d = map(int, sys.stdin.readline().split())
 distance = [i for i in range(10001)]
+g = []
     
 for i in range(n):
-    a, b, c = map(int, input().split())
-    g[a].append([b, c])
+    a, b, c = map(int, sys.stdin.readline().split())
+    g.append([a, b, c])
     
-print(dijkstra(0, distance))
+print(function(d, distance, g))
